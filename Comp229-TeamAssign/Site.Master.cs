92c6178bf2c63e77cad1488cac5b325912cd2fd2
SNandalan <snandal2@my.centennialcollege.ca>
2017-12-12 19:38:47 -0500

    Added Page_load to Site.Master code behind to automatically change page titles.

--- Comp229-TeamAssign/Site.Master.cs
+++ Comp229-TeamAssign/Site.Master.cs
@@ -11,7 +11,18 @@ namespace Comp229_TeamAssign
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    Page.Title = string.Format("Books Library");
+                    break;
+                case "About":
+                    Page.Title = string.Format("List of books");
+                    break;
+                case "Contact":
+                    Page.Title = string.Format("Contact Us");
+                    break;
+            }
         }
     }
 }
