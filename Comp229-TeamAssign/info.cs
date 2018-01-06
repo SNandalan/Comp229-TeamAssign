59a1316ac355c4afbd4980bfa9c146b9ca930798
SNandalan <snandal2@my.centennialcollege.ca>
2018-01-05 23:29:41 -0500

    Added Connection and info classes

---
+++ Comp229-TeamAssign/info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comp229_TeamAssign
+{
+    public class Info
+    {
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public Info(int infoID, string name, string lastName, string phoneNumber, string address)
+        {
+            ClientID = infoID;
+            FirstName = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+}
