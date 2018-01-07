f4c254e8c51a8193600ee4cf69f095c16824dd03
chanpreet742 <chanpreet742@gmail.com>
2018-01-06 23:17:34 -0500

    Added more description in the Word Document.

--- Comp229-TeamAssign/Info.cs
+++ Comp229-TeamAssign/Info.cs
@@ -9,11 +9,11 @@ namespace Comp229_TeamAssign
     {
         public string BookTitle { get; set; }
         public string AuthorName { get; set; }
-        public string ISBN { get; set; }
+        public int ISBN { get; set; }
         public string Genre { get; set; }
-        public string Rating { get; set; }
+        public double Rating { get; set; }
 
-        public Info(string bookTitle, string authorName, string isbn, string genre, string rating)
+        public Info(string bookTitle, string authorName, int isbn, string genre, double rating)
         {
             BookTitle = bookTitle;
             AuthorName = authorName;
